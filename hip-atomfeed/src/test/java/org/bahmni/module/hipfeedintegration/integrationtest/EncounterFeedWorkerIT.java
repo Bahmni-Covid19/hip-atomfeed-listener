ffdc71038d3a7e5f4d7c673d72823934385f9fd8
swatigogia2020 <swati.gogia@thoughtworks.com>
2022-11-25 14:18:47 +0530

    BAH-2549: Initial changes to resolve security vulnerability

--- hip-atomfeed/src/test/java/org/bahmni/module/hipfeedintegration/integrationtest/EncounterFeedWorkerIT.java
+++ hip-atomfeed/src/test/java/org/bahmni/module/hipfeedintegration/integrationtest/EncounterFeedWorkerIT.java
@@ -16,7 +16,8 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
@@ -32,7 +33,7 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = HipFeedListener.class)
+@SpringBootTest(classes = HipFeedListener.class)
 public class EncounterFeedWorkerIT extends OpenMRSMapperBaseTest{
 
     @InjectMocks
