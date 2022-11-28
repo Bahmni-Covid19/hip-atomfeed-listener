f390c7a5d904898d90aa1c79ee209f0b0926eec0
swatigogia2020 <swati.gogia@thoughtworks.com>
2022-11-28 13:25:42 +0530

    BAH-2549: Commenting out the failing test cases

--- hip-atomfeed/src/test/java/org/bahmni/module/hipfeedintegration/services/OpenMRSServiceTest.java
+++ hip-atomfeed/src/test/java/org/bahmni/module/hipfeedintegration/services/OpenMRSServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -23,6 +24,7 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 @PrepareForTest(WebClientFactory.class)
 @RunWith(PowerMockRunner.class)
+@PowerMockIgnore("jdk.internal.reflect.*")
 public class OpenMRSServiceTest extends OpenMRSMapperBaseTest {
 
     @Mock
